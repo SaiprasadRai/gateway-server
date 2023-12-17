d0b4e6f2f780db94da1ab2f8d60e3213cd60e4a4
Saiprasad Rai <saiprasadmrai9993@gmail.com>
2023-12-17 23:14:15 +0530

    implementation of spring-web -> v6.12

--- src/main/java/com/gateway/server/gatewayserver/controller/GatewayServerController.java
+++ src/main/java/com/gateway/server/gatewayserver/controller/GatewayServerController.java
@@ -1,4 +1,15 @@
 package com.gateway.server.gatewayserver.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GatewayServerController {
+
+    @RequestMapping
+    public Object getInfo()
+    {
+        //this is about to be built;
+        return null;
+    }
 }
